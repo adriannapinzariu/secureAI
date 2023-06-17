5cdfdaaf8e884c85d5a501246c95ce01a58851da
audgeviolin07 <123830780+audgeviolin07@users.noreply.github.com>
2023-06-17 15:15:39 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Reports from './reports';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reports" element={<Reports/>} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/reports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1>Reports Page</h1>
+      <p>This is the Reports page of our website.</p>
+    </div>
+  );
+};
+
+export default Reports;
