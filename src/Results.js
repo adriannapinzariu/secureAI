784a36e9d7beb7ed146dc16e668c5aaef475cda7
“arfamomin” <arfamomin@Arfas-MacBook-Pro.local>
2023-06-18 02:18:41 -0500

    uploading image feature added on results page

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 
 const Results = () => {
+  const [uploadedImage, setUploadedImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setUploadedImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>
-      <h1>Results Page</h1>
-      <button className="upload-button">Upload</button>
+    <div className="results-container">
+      {/* <h1 className="title">Find the security score for this image</h1> */}
+      <div className="image-container">
+        {uploadedImage ? (
+          <img className="uploaded-image" src={uploadedImage} alt="Uploaded" />
+        ) : (
+          <img className="uploaded-image" src="uploading-img.png" alt="Uploading" />
+        )}
+      </div>
+      <div className="upload-container">
+        <input type="file" onChange={handleImageUpload} className="upload-button" accept="image/*" />
+      </div>
     </div>
   );
 };
